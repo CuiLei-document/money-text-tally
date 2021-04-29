332861755cb48250d66817b334e8fc789b7b9f5a
磊哥 <493114008@qq.com>
2021-04-29 16:55:02 +0800

    deploy

---
+++ precache-manifest.92b209c061210f9b1ac795829a9321c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6465f68081f50f53a45b",
+    "url": "/money-text-tally/css/app.bdcf6665.css"
+  },
+  {
+    "revision": "4baa2198523d5e3c1fe81d93076e10ab",
+    "url": "/money-text-tally/index.html"
+  },
+  {
+    "revision": "6465f68081f50f53a45b",
+    "url": "/money-text-tally/js/app.7cb532a1.js"
+  },
+  {
+    "revision": "e09d0a72909deeb4d977",
+    "url": "/money-text-tally/js/chunk-vendors.88dfa304.js"
+  },
+  {
+    "revision": "adfc3cfc80a92b9b42d7494f4e120218",
+    "url": "/money-text-tally/manifest.json"
+  },
+  {
+    "revision": "145d0530ad73e83c9e0a7e3258ee4d49",
+    "url": "/money-text-tally/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-text-tally/robots.txt"
+  }
+]);
